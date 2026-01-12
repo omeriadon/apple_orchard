684f6297afba5b93f90d77bc01fea622932460e5
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-01-12 21:15:09 +0800

    again a small refactor

--- src/components/ScrollTimeline.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {
-	children: React.ReactNode;
-};
-
-export default function ScrollTimeline({ children }: Props) {
-	return <div role="list">{children}</div>;
-}
