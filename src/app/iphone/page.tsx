adf9703e1fafe2faa76460f784a726371444cc5f
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-01-13 07:30:25 +0800

    css update

--- src/app/iphone/page.tsx
+++ src/app/iphone/page.tsx
@@ -64,7 +64,7 @@ export default function Page() {
 									return next;
 								});
 								setActiveId((cur) =>
-									cur === device.id ? null : cur,
+									cur === device.id ? null : cur
 								);
 							}}
 						/>
